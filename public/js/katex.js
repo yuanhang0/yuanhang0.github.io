8cf58417ca15acdf7f41e2f21721409a62e1bac7
yuanhang0 <41170526+yuanhang0@users.noreply.github.com>
2019-07-29 21:55:18 -0400

    support both inline and display mode

--- public/js/katex.js
+++ public/js/katex.js
@@ -2,29 +2,27 @@
   layout: null
 ---
   {% if site.katex.enable %}
-$("<link>").attr({ href: "{{ site.katex.stylesheet }}", rel: "stylesheet" }).appendTo("head");
-$.ajax({
-  url: "{{ site.katex.src }}",
-  dataType: "script",
-  cache: true
-});
-$.ajax({
-  url: "{{ site.katex.auto-render }}",
-  dataType: "script",
-  cache: true
-});
-{% if site.katex.delimiters %}
-  $(window).on("load", function () {
-    renderMathInElement(document.body,
-      {
-        delimiters: [
-          {left: "$$", right: "$$", displayMode: true},
-          {left: "\\[", right: "\\]", displayMode: true},
-          {left: "$", right: "$", displayMode: false},
-          {left: "\\(", right: "\\)", displayMode: false}
-        ]
-      }
-    );
-  });
-{% endif %}
-{% endif %}
+    $("<link>").attr({ href: "{{ site.katex.stylesheet }}", rel: "stylesheet" }).appendTo("head");
+    $.ajax({
+      url: "{{ site.katex.src }}",
+      dataType: "script",
+      cache: true
+    });
+    $.ajax({
+      url: "{{ site.katex.auto-render }}",
+      dataType: "script",
+      cache: true
+    });
+      $(window).on("load", function () {
+        renderMathInElement(document.body,
+          {
+            delimiters: [
+              {left: "$$", right: "$$", displayMode: true},
+              {left: "\\[", right: "\\]", displayMode: true},
+              {left: "$", right: "$", displayMode: false},
+              {left: "\\(", right: "\\)", displayMode: false}
+            ]
+          }
+        );
+      });
+  {% endif %}
